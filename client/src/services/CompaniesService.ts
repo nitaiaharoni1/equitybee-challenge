da1b41f5f9f7fbedca66f7fda5f7ff2040deefcb
nitaiaharoni1 <nitaiaharoni1@gmail.com>
2022-06-09 18:36:00 +0300

    added

--- client/src/services/CompaniesService.ts
+++ client/src/services/CompaniesService.ts
@@ -1,15 +1,5 @@
 import axios from 'axios';
 
-// export const fetchCompany = (domain: string): Promise<ICompany> | undefined => axios.get(`/companies/${domain}`);
-export const fetchCompany = async (domain: string) => {
-  const res = await axios.get(`https://company.clearbit.com/v2/companies/find?domain=${domain}`, {
-    headers: {
-      Authorization: 'Bearer sk_30240e2d1dfc1d73d26ab80390d1fd49',
-    },
-  });
-  if (!res) {
-    console.error('Error fetching company');
-    return undefined;
-  }
-  return res.data;
-};
+import { ICompany } from '../types/interfaces/ICompany';
+
+export const fetchCompany = (domain: string): Promise<ICompany> | undefined => axios.get(`/companies/${domain}`);
